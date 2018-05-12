d7e622a07f0e39379c16184508149d4f1815e18b
Raymart De Guzman <raymart54@gmail.com>
2018-05-12 14:49:16 -0400

    First commit

---
+++ src/main/java/com/cv/parser/App.java
@@ -0,0 +1,13 @@
+package com.cv.parser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
