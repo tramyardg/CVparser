85bd9feb761edc2bd051d681895761c5805de728
Raymart De Guzman <raymart54@gmail.com>
2018-05-13 11:26:21 -0400

    new class to validate before read

---
+++ src/main/java/com/cv/parser/read/ValidateRead.java
@@ -0,0 +1,56 @@
+package com.cv.parser.read;
+
+import java.io.File;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.swt.widgets.Shell;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.cv.parser.CVparserSingleton;
+import com.cv.parser.extract.TXTExtractor;
+
+public class ValidateRead {
+    Logger logger = LoggerFactory.getLogger(TXTExtractor.class);
+    
+    File resumesStoragePath = new File(CVparserSingleton.getInstance().resumesStoragePath);
+    Shell shell;
+    
+    public ValidateRead(Shell shell)  {
+	this.shell = shell;
+    }
+    
+    private boolean isPublicDirContainsResumes() {
+	if (this.resumesStoragePath.listFiles().length == 0) {
+	    MessageBox messageBox = new MessageBox(shell, SWT.ICON_WARNING);
+	    messageBox.setText("Warning");
+	    messageBox.setMessage("No resume files found in public directory.");
+
+	    int buttonID = messageBox.open();
+	    if (buttonID == SWT.OK) {
+		logger.error("No resumes found");
+	    }
+	    return false;
+	} else {
+	    return true;
+	}
+    }
+
+    public void validateBeforeRead() {
+	if (!this.resumesStoragePath.exists()) {
+	    MessageBox messageBox = new MessageBox(shell, SWT.ICON_ERROR);
+	    messageBox.setText("Error");
+	    messageBox.setMessage("Directory public not found!");
+
+	    int buttonID = messageBox.open();
+	    if (buttonID == SWT.OK) {
+		logger.error("Directory public not found!");
+	    }
+	} else {
+	    if (!isPublicDirContainsResumes()) {
+		return;
+	    }
+	}
+    }
+}
